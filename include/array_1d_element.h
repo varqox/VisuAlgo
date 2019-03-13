b381d03c078505e10b8c12d46d207e148460e458
Filip Smoleński <philip.smolenski1403@gmail.com>
2019-03-13 18:15:49 +0100

    Basic table 1D

--- include/array_1d_element.h
+++ include/array_1d_element.h
@@ -9,22 +9,45 @@ namespace valgo {
 template<class T>
 class Array1DElement : public SlideElement {
 private:
-	std::vector<T> vec_;
+    std::vector<T> vec_;
 
 public:
-	Array1DElement() = default;
+    Array1DElement() = default;
 
-	virtual std::unique_ptr<SlideElement> clone() const override;
+    virtual std::unique_ptr<SlideElement> clone() const override {
+        return std::make_unique<Array1DElement>(*this);
+    }
 
-	void set(std::vector<T> vec) noexcept { vec_ = std::move(vec); }
+    void set(std::vector<T> vec) noexcept { vec_ = std::move(vec); }
 
-	void resize(size_t n) { vec_.resize(n); }
+    void resize(size_t n) { vec_.resize(n); }
 
-	void set_elem(size_t n, T val) { vec_[n] = std::move(val); }
+    void set_elem(size_t n, T val) { vec_[n] = std::move(val); }
 
-	virtual LatexCode draw_as_latex() const override;
+    virtual LatexCode draw_as_latex() const override {
+        std::string ret = "\\begin{table}[h!]\n \\begin{tabular}{|";
+        
+        for (size_t i = 0; i < vec_.size(); i++)
+            ret += "c|";
+        ret += "}\n  \\hline\n  ";
+        
+        for (size_t i = 0; i < vec_.size(); i++) {
+            if (i < vec_.size() - 1)  {
+                ret += " " + std::to_string(vec_[i]) + " &";
+            }
+            else {
+                ret += " " + std::to_string(vec_[i]) + "\\\\\n  \\hline\n";
+            }
+        }
 
-	virtual HTMLCode draw_as_html() const override;
+        ret += " \\end{tabular}\n \\end{table}\n";
+
+        return ret;
+    }
+
+    virtual HTMLCode draw_as_html() const override {
+        throw NotImplemented();
+    }
 };
 
 } // namespace valgo
