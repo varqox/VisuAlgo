f37d73957a85cff8d5f4f16398c03c72c5d84234
Filip Smoleński <philip.smolenski1403@gmail.com>
2019-03-14 23:22:23 +0100

    Basic 2D array

--- include/array_2d_element.h
+++ include/array_2d_element.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "slide_element.h"
-
+#include <sstream>
 #include <vector>
 
 namespace valgo {
@@ -15,17 +15,50 @@ private:
 public:
 	Array2DElement() = default;
 
-	virtual std::unique_ptr<SlideElement> clone() const override;
-
-	void resize(size_t n, size_t m) { vec_.resize(n * m); }
-
-	void set_elem(size_t i, size_t j, T val) {
-		vec_[i * m_ + j] = std::move(val);
+	virtual std::unique_ptr<SlideElement> clone() const override {
+		 return std::make_unique<Array2DElement>(*this);
 	}
 
-	virtual LatexCode draw_as_latex() const override;
+	void resize(size_t n, size_t m) { 
+		vec_.resize(n * m);
+		n_ = n;
+		m_ = m;
+	}
 
-	virtual HTMLCode draw_as_html() const override;
+	void set_elem(size_t i, size_t j, T val) { 
+		vec_[i * n_ + j] = std::move(val);
+	}
+
+	virtual LatexCode draw_as_latex() const override {
+		std::stringstream ret;
+		ret << "\\begin{table}[h!]\n \\begin{tabular}{|";
+
+		for (size_t i = 0; i < m_; i++)
+            ret << "c|";
+        ret << "}\n  \\hline\n  ";
+
+        for (size_t i = 0; i < n_; i++) {
+        	for (size_t j = 0; j < m_; j++) {
+        		if (j < m_ - 1)  {
+                	ret << " " << vec_[i * n_ + j] << " &";
+            	}
+            	else {
+                	ret << " " << vec_[i * n_ + j] << "\\\\\n  \\hline\n";
+            	}
+        	}
+
+        	if (i != n_ - 1)
+        		ret << "  ";
+        }
+
+        ret << " \\end{tabular}\n \\end{table}\n";
+
+        return ret.str();
+	}
+
+	virtual HTMLCode draw_as_html() const override {
+        throw NotImplemented();
+    }
 };
 
 } // namespace valgo
