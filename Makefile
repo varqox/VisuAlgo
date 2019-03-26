7a2329b7d01917c967adcaefa984f6f9931d9f87
Krzysztof Małysa <krzyk240@gmail.com>
2019-03-27 00:02:27 +0100

    Added unit tests of Latex; Added Google Test as a submodule;

--- Makefile
+++ Makefile
@@ -4,6 +4,18 @@ include Makefile.config
 all: visualgo.a hierarchy.svg examples
 	@printf "\033[32mBuild finished\033[0m\n"
 
+GOOGLETEST_SRCS := \
+	$(PREFIX)googletest/googletest/src/gtest-all.cc \
+	$(PREFIX)googletest/googletest/src/gtest_main.cc
+
+$(eval $(call load_dependencies, $(GOOGLETEST_SRCS)))
+GOOGLETEST_OBJS := $(call SRCS_TO_OBJS, $(GOOGLETEST_SRCS))
+
+gtest_main.a: $(GOOGLETEST_OBJS)
+	$(MAKE_STATIC_LIB)
+
+$(GOOGLETEST_OBJS): override EXTRA_CXX_FLAGS += -isystem '$(CURDIR)/googletest/googletest/include' -I '$(CURDIR)/googletest/googletest' -pthread
+
 VISUALGO_SRCS := \
 	src/block.cc \
 	src/color.cc \
@@ -63,12 +75,27 @@ examples/examples.pdf: $(EXAMPLES_PDFS)
 	$(Q)pdfunite $(EXAMPLES_PDFS) $@
 	@printf "\033[32mCombined generated pdfs into \033[1;32m$@\033[0m\n"
 
+VISUALGO_TEST_SRCS := \
+	test/latex.cc
+
+$(eval $(call load_dependencies, $(VISUALGO_TEST_SRCS)))
+VISUALGO_TEST_OBJS := $(call SRCS_TO_OBJS, $(VISUALGO_TEST_SRCS))
+
+$(VISUALGO_TEST_OBJS): override EXTRA_CXX_FLAGS += -isystem '$(CURDIR)/googletest/googletest/include'
+
+test/exec: $(VISUALGO_TEST_OBJS) visualgo.a gtest_main.a
+	$(LINK) -pthread
+
+.PHONY: test
+test: test/exec
+	test/exec
+
 .PHONY: clean
-clean: OBJS := $(VISUALGO_OBJS) $(EXAMPLES_OBJS)
+clean: OBJS := $(GOOGLETEST_OBJS) $(VISUALGO_OBJS) $(EXAMPLES_OBJS) $(VISUALGO_TEST_OBJS)
 clean:
-	$(Q)$(RM) $(OBJS) $(OBJS:o=dwo) visualgo.a $(EXAMPLES_EXECS) $(EXAMPLES_PDFS) examples/examples.pdf
+	$(Q)$(RM) $(OBJS) $(OBJS:o=dwo) gtest_main.a visualgo.a $(EXAMPLES_EXECS) $(EXAMPLES_PDFS) examples/examples.pdf
 	$(Q)find examples -type f -name '*.tex' | xargs rm -f
-	$(Q)find src -type f -name '*.deps' | xargs rm -f
+	$(Q)find src googletest test -type f -name '*.deps' | xargs rm -f
 
 .PHONY: help
 help:
---
+++ test/latex.cc
@@ -0,0 +1,77 @@
+#include "../include/latex.h"
+
+#include <gtest/gtest.h>
+
+using namespace valgo;
+
+TEST (Latex, clone) {
+	// LaTeX backend
+	EXPECT_EQ(Latex().clone()->draw_as_latex(), "");
+
+	{
+		Latex latex("lorem ipsum");
+		EXPECT_EQ(latex.clone()->draw_as_latex(), "lorem ipsum");
+		latex.set("foo bar");
+		EXPECT_EQ(latex.clone()->draw_as_latex(), "foo bar");
+	}
+
+	{
+		Latex latex("$e=mc^2$");
+		EXPECT_EQ(latex.clone()->draw_as_latex(), "$e=mc^2$");
+		latex.set("\\newline");
+		EXPECT_EQ(latex.clone()->draw_as_latex(), "\\newline");
+	}
+
+	// HTML backend
+	EXPECT_THROW(Latex().clone()->draw_as_html(), NotImplemented);
+
+	{
+		Latex latex("lorem ipsum");
+		EXPECT_THROW(latex.clone()->draw_as_html(), NotImplemented);
+		latex.set("foo bar");
+		EXPECT_THROW(latex.clone()->draw_as_html(), NotImplemented);
+	}
+}
+
+TEST (Latex, set) {
+	{
+		Latex latex("lorem ipsum");
+		latex.set("foo bar"); // Setting on value-initialized latex
+		EXPECT_EQ(latex.draw_as_latex(), "foo bar");
+	}
+
+	{
+		Latex latex;
+		latex.set("$e=mc^2$"); // Setting on empty-initialized Latex
+		EXPECT_EQ(latex.draw_as_latex(), "$e=mc^2$");
+		latex.set("\\newline");
+		EXPECT_EQ(latex.draw_as_latex(), "\\newline");
+	}
+}
+
+TEST (Latex, draw_as_latex) {
+	EXPECT_EQ(Latex().draw_as_latex(), "");
+
+	{
+		Latex latex("lorem ipsum");
+		EXPECT_EQ(latex.draw_as_latex(), "lorem ipsum");
+		latex.set("foo bar");
+		EXPECT_EQ(latex.draw_as_latex(), "foo bar");
+	}
+
+	{
+		Latex latex("$e=mc^2$");
+		EXPECT_EQ(latex.draw_as_latex(), "$e=mc^2$");
+		latex.set("\\newline");
+		EXPECT_EQ(latex.draw_as_latex(), "\\newline");
+	}
+}
+
+TEST (Latex, draw_as_html) {
+	EXPECT_THROW(Latex().draw_as_html(), NotImplemented);
+
+	Latex latex("lorem ipsum");
+	EXPECT_THROW(latex.draw_as_html(), NotImplemented);
+	latex.set("foo bar");
+	EXPECT_THROW(latex.draw_as_html(), NotImplemented);
+}
