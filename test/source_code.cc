c385a693d410dfd6b53a50bc722eefd350761e1d
pbrw <borowski.pb1@gmail.com>
2019-03-27 20:59:46 +0100

    Added unit tests of SourceCode

--- test/source_code.cc
+++ test/source_code.cc
@@ -1,4 +1,40 @@
-//
-// Created by piotr on 27.03.19.
-//
+#include "../include/source_code.h"
+
+#include <gtest/gtest.h>
+
+using namespace valgo;
+
+TEST (SourceCode, set) {
+	SourceCode code;
+	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=,numbers=left,xleftmargin=16pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=6pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
+"\\end{lstlisting}");
+	code.set_lang("C++");
+	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,numbers=left,xleftmargin=16pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=6pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
+							   "\\end{lstlisting}");
+	code.set_code(
+		"long long sp(long long p, long long w) { \n"
+		"    if (w == 0) return 1;\n"
+		"    if (w % 2) return sp(p, w - 1) * p;\n"
+		"    long long f = sp(p, w / 2);\n"
+		"    return f * f;\n"
+		"}");
+
+	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,numbers=left,xleftmargin=16pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=6pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
+"long long sp(long long p, long long w) { \n"
+"    if (w == 0) return 1;\n"
+"    if (w % 2) return sp(p, w - 1) * p;\n"
+"    long long f = sp(p, w / 2);\n"
+"    return f * f;\n"
+"}\\end{lstlisting}");
+
+	code.set_title("Szybkie potegowanie");
+
+	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,title=Szybkie potegowanie,numbers=left,xleftmargin=16pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=6pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
+"long long sp(long long p, long long w) { \n"
+"    if (w == 0) return 1;\n"
+"    if (w % 2) return sp(p, w - 1) * p;\n"
+"    long long f = sp(p, w / 2);\n"
+"    return f * f;\n"
+"}\\end{lstlisting}");
+}
 
