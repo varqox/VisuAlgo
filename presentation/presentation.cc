09cabec20d0ae56418f4c8b0bcf8847d79a72040
Filip Smoleński <philip.smolenski1403@gmail.com>
2019-06-10 22:13:16 +0200

    next slides

--- presentation/presentation.cc
+++ presentation/presentation.cc
@@ -25,7 +25,28 @@ void add_diagrams(LatexPresentation& pres) {
 		itemz.add_item(Latex("organizator konkursu algorytmicznego tworzący omówienie zadań"));
 		itemz.add_item(Latex("osoba tworząca internetowy kurs STLa"));
 		itemz.add_item(Latex("pracownik naukowy publikujący własny algorytm"));
-		pres.add_slide(Slide(itemz).set_title("GRUPY UŻYTKOWNIKÓW / ROLE W SYSTEMIE"));
+		pres.add_slide(Slide(itemz).set_title("Grupy użytkowników / Role w systemie"));
+	}
+	{
+		Itemize itemz;
+		itemz.add_item(Latex("Tablice 1D, Tablice 2D"));
+		itemz.add_item(Latex("Inline LaTeX, zmienne, kod źródłowy"));
+		itemz.add_item(Latex("Grafy (nieskierowane, skierowane, dagi, drzewa, grafy funkcyjne)"));
+		itemz.add_item(Latex("Kontenery"));
+		itemz.add_item(Latex("Macierze"));
+		itemz.add_item(Latex("Geometria"));
+		itemz.add_item(Latex("Obrazki"));
+		pres.add_slide(Slide(itemz).set_title("Funkcjonalności"));
+	}
+	{
+		Itemize itemz;
+		itemz.add_item(Latex("C++"));
+		itemz.add_item(Latex("Graphviz"));
+		itemz.add_item(Latex("Github"));
+		itemz.add_item(Latex("GoogleTest"));
+		itemz.add_item(Latex("Doxygen"));
+		itemz.add_item(Latex("LaTeX"));
+		pres.add_slide(Slide(itemz).set_title("Narzędzia i technologie"));
 	}
 	{
 		Image im("./io_stuff/architecture/" ,"Logical_View.pdf");
@@ -58,6 +79,13 @@ void add_diagrams(LatexPresentation& pres) {
 		ImageSet imset;
 		imset.add_image(im);
 		pres.add_slide(Slide(imset).set_title("Architecture - use case diagram"));
+	}
+	{
+		Itemize itemz;
+		itemz.add_item(Latex("Webaplikacja"));
+		itemz.add_item(Latex("Konwersja do kodu html"));
+		itemz.add_item(Latex("Robienie grafów z użyciem tikza"));
+		pres.add_slide(Slide(itemz).set_title("Dalsze możliwości rozwoju aplikacji"));
 	}
 }
 
