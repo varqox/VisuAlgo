514153f7f1916f43456455ae77542b9db89ea022
Krzysztof Małysa <krzyk240@gmail.com>
2019-06-12 23:13:57 +0200

    Fixed source code tests

--- test/source_code.cc
+++ test/source_code.cc
@@ -6,10 +6,10 @@ using namespace valgo;
 
 TEST (SourceCode, set) {
 	SourceCode code;
-	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=,numbers=left,xleftmargin=16pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=6pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
+	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=,numbers=left,aboveskip=-2pt,belowskip=0pt,xleftmargin=12pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=5pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
 "\\end{lstlisting}");
 	code.set_lang("C++");
-	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,numbers=left,xleftmargin=16pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=6pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
+	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,numbers=left,aboveskip=-2pt,belowskip=0pt,xleftmargin=12pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=5pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
 							   "\\end{lstlisting}");
 	code.set_code(
 		"long long sp(long long p, long long w) { \n"
@@ -19,7 +19,7 @@ TEST (SourceCode, set) {
 		"    return f * f;\n"
 		"}");
 
-	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,numbers=left,xleftmargin=16pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=6pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
+	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,numbers=left,aboveskip=-2pt,belowskip=0pt,xleftmargin=12pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=5pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
 "long long sp(long long p, long long w) { \n"
 "    if (w == 0) return 1;\n"
 "    if (w % 2) return sp(p, w - 1) * p;\n"
@@ -29,7 +29,7 @@ TEST (SourceCode, set) {
 
 	code.set_title("Szybkie potegowanie");
 
-	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,title=Szybkie potegowanie,numbers=left,xleftmargin=16pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=6pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
+	EXPECT_EQ(code.draw_as_latex(), "\\begin{lstlisting}[language=C++,title=Szybkie potegowanie,numbers=left,aboveskip=-2pt,belowskip=0pt,xleftmargin=12pt,basicstyle=\\small\\ttfamily,showstringspaces=false,numbersep=5pt,numberstyle=\\small\\ttfamily,columns=spaceflexible,keepspaces=true,tabsize=4,breaklines=true,commentstyle=\\color{gray},keywordstyle=\\color{blue}\\bfseries,stringstyle=\\color{purple}]\n"
 "long long sp(long long p, long long w) { \n"
 "    if (w == 0) return 1;\n"
 "    if (w % 2) return sp(p, w - 1) * p;\n"
