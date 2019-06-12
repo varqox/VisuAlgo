f76a70430c5881a47f54804915d402ebfc5ac4e2
tropuq <quport@gmail.com>
2019-06-12 21:43:39 +0200

    Fixed some unit tests

--- test/variable.cc
+++ test/variable.cc
@@ -8,25 +8,25 @@ TEST (Variable, set) {
 	{
 		Variable<int> variable("magic");
 		variable.set(42);
-		EXPECT_EQ(variable.draw_as_latex(), "magic = 42");
+		EXPECT_EQ(variable.draw_as_latex(), "magic = 42\\\\\n");
 		variable.set(43);
-		EXPECT_EQ(variable.draw_as_latex(), "magic = 43");
+		EXPECT_EQ(variable.draw_as_latex(), "magic = 43\\\\\n");
 	}
 
 	{
 		Variable<double> variable("pi");
 		variable.set(3.14159);
-		EXPECT_EQ(variable.draw_as_latex(), "pi = 3.14159");
+		EXPECT_EQ(variable.draw_as_latex(), "pi = 3.14159\\\\\n");
 		variable.set(2.781);
-		EXPECT_EQ(variable.draw_as_latex(), "pi = 2.781");
+		EXPECT_EQ(variable.draw_as_latex(), "pi = 2.781\\\\\n");
 	}
 
 	{
 		Variable<std::string> variable("thue");
 		variable.set("abba");
-		EXPECT_EQ(variable.draw_as_latex(), "thue = abba");
+		EXPECT_EQ(variable.draw_as_latex(), "thue = abba\\\\\n");
 		variable.set("j_98g:Gg)d");
-		EXPECT_EQ(variable.draw_as_latex(), "thue = j_98g:Gg)d");
+		EXPECT_EQ(variable.draw_as_latex(), "thue = j_98g:Gg)d\\\\\n");
 	}
 
 	{
@@ -42,25 +42,25 @@ TEST (Variable, setAndClone) {
 	{
 		Variable<int> variable("magic");
 		variable.set(42);
-		EXPECT_EQ(variable.clone()->draw_as_latex(), "magic = 42");
+		EXPECT_EQ(variable.clone()->draw_as_latex(), "magic = 42\\\\\n");
 		variable.set(43);
-		EXPECT_EQ(variable.clone()->draw_as_latex(), "magic = 43");
+		EXPECT_EQ(variable.clone()->draw_as_latex(), "magic = 43\\\\\n");
 	}
 
 	{
 		Variable<double> variable("pi");
 		variable.set(3.14159);
-		EXPECT_EQ(variable.clone()->draw_as_latex(), "pi = 3.14159");
+		EXPECT_EQ(variable.clone()->draw_as_latex(), "pi = 3.14159\\\\\n");
 		variable.set(2.781);
-		EXPECT_EQ(variable.clone()->draw_as_latex(), "pi = 2.781");
+		EXPECT_EQ(variable.clone()->draw_as_latex(), "pi = 2.781\\\\\n");
 	}
 
 	{
 		Variable<std::string> variable("thue");
 		variable.set("abba");
-		EXPECT_EQ(variable.clone()->draw_as_latex(), "thue = abba");
+		EXPECT_EQ(variable.clone()->draw_as_latex(), "thue = abba\\\\\n");
 		variable.set("j_98g:Gg)d");
-		EXPECT_EQ(variable.clone()->draw_as_latex(), "thue = j_98g:Gg)d");
+		EXPECT_EQ(variable.clone()->draw_as_latex(), "thue = j_98g:Gg)d\\\\\n");
 	}
 
 	{
